7af05470f1581d5dabe5322c29c4b5e8028cb6e1
arunprajapati-arch <arunprajapati.mail@gmail.com>
2024-10-28 09:49:44 +0000

    add shadcn & acertinity ui

---
+++ next-app/next.config.mjs
@@ -0,0 +1,11 @@
+// next.config.mjs
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+      domains: ['assets.aceternity.com'], // Add the hostname here
+    },
+  };
+  
+  export default nextConfig;
+  
---
+++ next-app/src/app/page.tsx
@@ -0,0 +1,10 @@
+import ExpandableCardDemo from "@/components/blocks/expandable-card-demo-grid";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+    <ExpandableCardDemo/>
+    </>
+  );
+}
